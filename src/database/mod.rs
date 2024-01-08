540d2ef44735ea6aee4b319f9915395c6892c6df
hoonyland <hoonyland.newsletter@gmail.com>
2024-01-08 15:52:00 +0100

    fix: handle updated response of reqwest

--- src/database/mod.rs
+++ src/database/mod.rs
@@ -1,7 +1,7 @@
 use crate::client;
 use async_trait::async_trait;
 use serde::Serialize;
-use std::fmt::{Debug};
+use std::fmt::Debug;
 use std::marker::Send;
 
 pub mod models;
@@ -9,17 +9,17 @@ pub mod supabase;
 
 #[async_trait]
 pub trait DatabaseClient {
-  async fn get(
-    &self,
-    table_name: &str,
-    query: Option<&str>,
-  ) -> Result<reqwest::Response, reqwest::Error>;
+    async fn get(
+        &self,
+        table_name: &str,
+        query: Option<&str>,
+    ) -> Result<reqwest::Response, reqwest::Error>;
 
-  async fn post<T: Debug + Serialize + Send>(
-    &self,
-    table_name: &str,
-    items: Vec<T>,
-  ) -> Result<reqwest::Response, reqwest::Error>;
+    async fn post<T: Debug + Serialize + Send>(
+        &self,
+        table_name: &str,
+        items: Vec<T>,
+    ) -> Result<reqwest::Response, reqwest::Error>;
 }
 
 // // TODO:
@@ -28,40 +28,44 @@ pub trait DatabaseClient {
 // }
 
 pub async fn create_bills<C: DatabaseClient>(
-  database_client: &C,
-  bills_from_api: &Vec<client::DtlVo>,
+    database_client: &C,
+    bills_from_api: &Vec<client::DtlVo>,
 ) -> Result<Vec<models::BillRow>, reqwest::Error> {
-  let bills = bills_from_api
-    .iter()
-    .map(|b| models::BillRow::new(b))
-    .collect();
+    let bills = bills_from_api
+        .iter()
+        .map(|b| models::BillRow::new(b))
+        .collect();
 
-  let response = database_client.post("bills", bills).await;
-  match response {
-    Ok(result) => {
-      if result.status() != 201 {
-        panic!("Supabase 업로드에 실패하였습니다\n상태코드: {}", result.status())
-      }
+    let response = database_client.post("bills", bills).await;
+    match response {
+        Ok(result) => {
+            let status_code = result.status().as_u16();
+            if status_code != 200 && status_code != 201 {
+                panic!(
+                    "Supabase 업로드에 실패하였습니다\n상태코드: {}",
+                    result.status()
+                )
+            }
 
-      result.json::<Vec<models::BillRow>>().await
+            result.json::<Vec<models::BillRow>>().await
+        }
+        Err(e) => {
+            eprintln!("{}", e);
+            Err(e)
+        }
     }
-    Err(e) => {
-      eprintln!("{}", e);
-      Err(e)
-    }
-  }
 }
 
 pub async fn find_bills<C: DatabaseClient>(
-  database_client: &C,
-  query: &str,
+    database_client: &C,
+    query: &str,
 ) -> Result<Vec<models::BillRow>, reqwest::Error> {
-  let response = database_client.get("bills", Some(query)).await;
-  match response {
-    Ok(result) => result.json::<Vec<models::BillRow>>().await,
-    Err(e) => {
-      eprintln!("{}", e);
-      Err(e)
+    let response = database_client.get("bills", Some(query)).await;
+    match response {
+        Ok(result) => result.json::<Vec<models::BillRow>>().await,
+        Err(e) => {
+            eprintln!("{}", e);
+            Err(e)
+        }
     }
-  }
 }
